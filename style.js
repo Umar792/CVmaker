00a9c8a46a56cf3f178f42a19aaf5dde5f369427
Umar792 <106830172+Umar792@users.noreply.github.com>
2022-10-03 14:00:17 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,84 @@
+const myfunction = () => {
+  let fname = document.getElementById("fname").value;
+  let fullName = document.getElementById("firstname");
+  fullName.innerHTML = fname;
+};
+
+// ==========================object
+
+const object = () => {
+  let myobject = document.getElementById("object").value;
+  let newObj = document.getElementById("obj");
+  newObj.innerHTML = myobject;
+};
+
+// ====================== mail
+
+const emailfunc = () => {
+  let email = document.getElementById("mail").value;
+  let newEmail = document.getElementById("myemail");
+  newEmail.innerHTML = email;
+};
+
+// ====================== mail
+
+const mobfunc = () => {
+  let mobile = document.getElementById("mob").value;
+  let newmobile = document.getElementById("mobile");
+  newmobile.innerHTML = mobile;
+};
+
+// ====================== mail
+
+const CNICfunc = () => {
+  let CNIC = document.getElementById("CNIC").value;
+  let newCNIC = document.getElementById("cnic");
+  newCNIC.innerHTML = CNIC;
+};
+
+// ====================== Adress
+
+const Adressfunc = () => {
+  let Adress = document.getElementById("Adress").value;
+  let newAdress = document.getElementById("adress");
+  newAdress.innerHTML = Adress;
+};
+
+// ====================== qualifation
+
+const Qualificationfunc = () => {
+  let Qualification = document.getElementById("Qualification").value;
+  let newQualification = document.getElementById("education");
+  newQualification.innerHTML = Qualification;
+};
+// ====================== Experience
+
+const Experiencefunc = () => {
+  let Experience = document.getElementById("Experience").value;
+  let newExperience = document.getElementById("experience");
+  newExperience.innerHTML = Experience;
+};
+
+// ====================== Code
+
+const Codefunc = () => {
+  let Code = document.getElementById("Code").value;
+  let newCode = document.getElementById("code");
+  newCode.innerHTML = Code;
+};
+
+// ====================== City
+
+const Cityfunc = () => {
+  let City = document.getElementById("City").value;
+  let newCity = document.getElementById("city");
+  newCity.innerHTML = City;
+};
+
+// ====================== Gender
+
+const Genderfunc = () => {
+  let CGenderity = document.getElementById("Gender").value;
+  let newGender = document.getElementById("gender");
+  newGender.innerHTML = CGenderity;
+};
